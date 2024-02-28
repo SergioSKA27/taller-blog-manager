40ac7c3ffc572b5c28509f95e84cde92ceac4a5a
SergioSKA27 <sergioska81@hotmail.com>
2024-02-28 03:14:20 -0600

    Actualiza el diseño de la página de mensajes y agrega funcionalidad para marcar y eliminar mensajes leídos.

--- pages/mensajes.py
+++ pages/mensajes.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from st_xatadb_connection  import XataConnection,XataClient
-import bcrypt
+import time
 
 
 client = XataClient(st.secrets['XATA_API_KEY'],db_url=st.secrets['XATA_DB_URL'])
-st.set_page_config(layout='centered')
+st.set_page_config(layout='wide')
 xata = st.connection('xata',type=XataConnection)
 
 
@@ -19,14 +19,34 @@ def update_messages():
     'filter':{
         'leido':{"$is": False}
     }})
+def update_read_messages():
+    st.session_state.read_messages = xata.query("Mensaje",{"columns": [
+        "emisor",
+        "correo",
+        "motivo",
+        "contenido",
+        "leido"
+    ],
+    'filter':{
+        'leido':{"$is": True}
+    }})
 def mark_as_read(message_id):
     try:
         response = xata.update("Mensaje",message_id,{"leido":True})
         #st.write(response)
         st.toast('Mensaje marcado como leido',icon='📩')
+        update_read_messages()
     except Exception as e:
         st.error(f'Error: {e}')
 
+def delete_message(message_id):
+    try:
+        xata.delete("Mensaje",message_id)
+        st.toast('Mensaje eliminado',icon='🗑️')
+        time.sleep(1)
+        update_read_messages()
+    except Exception as e:
+        st.error(f'Error: {e}')
 
 def render_message(message):
     st.write(f"De: {message['emisor']}")
@@ -34,7 +54,14 @@ def render_message(message):
     st.write(f"Motivo: {message['motivo']}")
     st.write(f"Mensaje: {message['contenido']}")
     if not message['leido']:
-        st.button('Marcar como leido',on_click=mark_as_read,args=(message['id'],))
+        if st.button('Marcar como leido',on_click=mark_as_read,args=(message['id'],),key=message['id']):
+            update_messages()
+            st.rerun()
+    else:
+        if st.button(':red[Eliminar]',on_click=delete_message,args=(message['id'],),key=message['id']):
+            st.rerun()
+
+
 
 
 
@@ -51,5 +78,44 @@ if 'messages' not in st.session_state:
     }
     })
 
+if 'read_messages' not in st.session_state:
+    st.session_state.read_messages = xata.query("Mensaje",{"columns": [
+        "emisor",
+        "correo",
+        "motivo",
+        "contenido",
+        "leido"
+    ],
+    'filter':{
+        'leido':{"$is": True}
+    }
+    })
 
 
+#st.write(st.session_state.messages)
+st.title('Mensajes')
+if st.button('Actualizar mensajes',on_click=update_messages,use_container_width=True):
+    update_read_messages()
+    st.rerun()
+cols = st.columns(4)
+k=0
+
+
+for i in st.session_state.messages['records']:
+    with cols[k]:
+        render_message(i)
+        k+=1
+        if k==4:
+            k=0
+
+
+st.header('Mensajes Leidos')
+cols2 = st.columns(4)
+t = 0
+
+for i in st.session_state.read_messages['records']:
+    with cols2[t]:
+        render_message(i)
+    t+=1
+    if t==4:
+        t=0
